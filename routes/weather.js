1dd574b4e6b91481deb6793ee028bc1698bc2661
breaker2l <34549956+breaker2l@users.noreply.github.com>
2020-07-20 21:00:36 -0700

    display api data

--- routes/weather.js
+++ routes/weather.js
@@ -4,52 +4,53 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 require('dotenv').config()
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
    // res.send('Hello From express');
    res.render('index', {
-       city:data.message,
-       des:null,
-       icon:null,
-       temp:null
+    city: null,
+    des: null,
+    icon: null,
+    temp:null
    });
 });
 
-router.post('/',async (req,res) => {
+router.post('/', async (req,res) => {
     const city = req.body.city;
-    const api = process.env.API_KEY;
-    const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
+    const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
     
     try {
-        await fetch(url_api)
+       await fetch(url_api)
         .then(res => res.json())
         .then(data => {
-        if(data.message === 'city not fonud') {
-           res.render('index', {
-               city: data.message,
-               des:null,
-               icon: null,
-               temp:null
-           })
-        } else {
-        const city = data.name;
-        const des = data.weather[0].description;
-        const icon = data.weather[0].icon;
-        const temp = data.main.temp;
+            if(data.message === 'city not found') {
+                res.render('index', {
+                    city: data.message,
+                    des: null,
+                    icon: null,
+                    temp:null
+                })
+            } else {
+                const city = data.name;
+                const des = data.weather[0].description;
+                const icon = data.weather[0].icon;
+                const temp = data.main.temp;
 
-        res.render('index',{
-            city,des,icon, temp
-        });
-    }
-    });
+                res.render('index',{
+                    city, des , icon, temp
+                });
+            }
 
+            });
+    
     } catch(err) {
-      res.render('index',{
-          city : 'something wrong',
-          des: null,
-          icon:null,
-          temp:null
-      })
+        res.render('index',{
+            city: 'something wrong',
+            des:null,
+            icon:null,
+            temp:null
+        })
     }
+
 })
     
 
